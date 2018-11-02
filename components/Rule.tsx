634e6c6fb3c80d6b7d7bb858653727d782a638cf
Gaelan Steele <gbs@canishe.com>
2018-11-02 09:26:25 -0700

    Add keywords.

--- components/Rule.tsx
+++ components/Rule.tsx
@@ -3,9 +3,18 @@ import {Rule as RuleType} from '../lib/ruleset-parser'
 import Styles from './Rule.css'
 import Markdown from 'react-markdown'
 import asciiRules from '../lib/ascii-rules.json'
+import keywords from '../lib/keywords.json'
+
+function addKeywords(text: string) {
+    Object.keys(keywords).forEach(keyword => {
+        text = text.replace(new RegExp("\\b" + keyword + "\\b"), `[${keyword}](#Rule${(keywords as {[keyword: string]: number})[keyword]})`)
+    })
+    return text
+}
 
 export default function Group({rule}: {rule: RuleType}) {
     return <div>
+        <a id={"Rule" + rule.id} />
         <h2 className={Styles.title}>
             <span className={Styles.id}>
                 {rule.id}<span className={Styles.rev}>/{rule.rev}</span>
@@ -14,7 +23,7 @@ export default function Group({rule}: {rule: RuleType}) {
             <span className={Styles.power}>Power {rule.power}</span>
         </h2>
         <div className={Styles.text}>
-            {asciiRules.includes(rule.id) ? <pre>{rule.text}</pre> : <Markdown source={rule.text} />}
+            {asciiRules.includes(rule.id) ? <pre>{rule.text}</pre> : <Markdown source={addKeywords(rule.text)} />}
         </div>
     </div>
 }
